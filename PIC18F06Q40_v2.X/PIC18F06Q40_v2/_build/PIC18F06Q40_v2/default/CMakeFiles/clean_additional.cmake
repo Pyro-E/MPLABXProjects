5c0a8154127419db532d27e50677a7b8e23d54ca
Pyro-E <101224264+Pyro-E@users.noreply.github.com>
2026-06-15 19:17:21 -0700

    v2

---
+++ PIC18F06Q40_v2.X/PIC18F06Q40_v2/_build/PIC18F06Q40_v2/default/CMakeFiles/clean_additional.cmake
@@ -0,0 +1,14 @@
+# Additional clean files
+cmake_minimum_required(VERSION 3.16)
+
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "")
+  file(REMOVE_RECURSE
+  "/Users/kevinlu/Code/Github/MPLABXProjects/PIC18F06Q40_v2.X/PIC18F06Q40_v2/out/PIC18F06Q40_v2/default.cmf"
+  "/Users/kevinlu/Code/Github/MPLABXProjects/PIC18F06Q40_v2.X/PIC18F06Q40_v2/out/PIC18F06Q40_v2/default.hex"
+  "/Users/kevinlu/Code/Github/MPLABXProjects/PIC18F06Q40_v2.X/PIC18F06Q40_v2/out/PIC18F06Q40_v2/default.hxl"
+  "/Users/kevinlu/Code/Github/MPLABXProjects/PIC18F06Q40_v2.X/PIC18F06Q40_v2/out/PIC18F06Q40_v2/default.mum"
+  "/Users/kevinlu/Code/Github/MPLABXProjects/PIC18F06Q40_v2.X/PIC18F06Q40_v2/out/PIC18F06Q40_v2/default.o"
+  "/Users/kevinlu/Code/Github/MPLABXProjects/PIC18F06Q40_v2.X/PIC18F06Q40_v2/out/PIC18F06Q40_v2/default.sdb"
+  "/Users/kevinlu/Code/Github/MPLABXProjects/PIC18F06Q40_v2.X/PIC18F06Q40_v2/out/PIC18F06Q40_v2/default.sym"
+  )
+endif()
---
+++ PIC18F06Q40_v2.X/PIC18F06Q40_v2/cmake/PIC18F06Q40_v2/default/.generated/main.cmake
@@ -0,0 +1,44 @@
+include("${CMAKE_CURRENT_LIST_DIR}/rule.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/file.cmake")
+
+set(PIC18F06Q40_v2_default_library_list )
+
+# Handle files with suffix (s|as|asm|AS|ASM|As|aS|Asm), for group default-XC8
+if(PIC18F06Q40_v2_default_default_XC8_FILE_TYPE_assemble)
+add_library(PIC18F06Q40_v2_default_default_XC8_assemble OBJECT ${PIC18F06Q40_v2_default_default_XC8_FILE_TYPE_assemble})
+    PIC18F06Q40_v2_default_default_XC8_assemble_rule(PIC18F06Q40_v2_default_default_XC8_assemble)
+    list(APPEND PIC18F06Q40_v2_default_library_list "$<TARGET_OBJECTS:PIC18F06Q40_v2_default_default_XC8_assemble>")
+
+endif()
+
+# Handle files with suffix S, for group default-XC8
+if(PIC18F06Q40_v2_default_default_XC8_FILE_TYPE_assemblePreprocess)
+add_library(PIC18F06Q40_v2_default_default_XC8_assemblePreprocess OBJECT ${PIC18F06Q40_v2_default_default_XC8_FILE_TYPE_assemblePreprocess})
+    PIC18F06Q40_v2_default_default_XC8_assemblePreprocess_rule(PIC18F06Q40_v2_default_default_XC8_assemblePreprocess)
+    list(APPEND PIC18F06Q40_v2_default_library_list "$<TARGET_OBJECTS:PIC18F06Q40_v2_default_default_XC8_assemblePreprocess>")
+
+endif()
+
+# Handle files with suffix [cC], for group default-XC8
+if(PIC18F06Q40_v2_default_default_XC8_FILE_TYPE_compile)
+add_library(PIC18F06Q40_v2_default_default_XC8_compile OBJECT ${PIC18F06Q40_v2_default_default_XC8_FILE_TYPE_compile})
+    PIC18F06Q40_v2_default_default_XC8_compile_rule(PIC18F06Q40_v2_default_default_XC8_compile)
+    list(APPEND PIC18F06Q40_v2_default_library_list "$<TARGET_OBJECTS:PIC18F06Q40_v2_default_default_XC8_compile>")
+
+endif()
+
+
+# Main target for this project
+add_executable(PIC18F06Q40_v2_default_image_f8QGpTQ1 ${PIC18F06Q40_v2_default_library_list})
+
+set_target_properties(PIC18F06Q40_v2_default_image_f8QGpTQ1 PROPERTIES
+    OUTPUT_NAME "default"
+    SUFFIX ".elf"
+    ADDITIONAL_CLEAN_FILES "${output_extensions}"
+    RUNTIME_OUTPUT_DIRECTORY "${PIC18F06Q40_v2_default_output_dir}")
+target_link_libraries(PIC18F06Q40_v2_default_image_f8QGpTQ1 PRIVATE ${PIC18F06Q40_v2_default_default_XC8_FILE_TYPE_link})
+
+# Add the link options from the rule file.
+PIC18F06Q40_v2_default_link_rule( PIC18F06Q40_v2_default_image_f8QGpTQ1)
+
+
